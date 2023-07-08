4b8412271560adf90abde1b9f4426017aa1bad62
NashiraDeer <nashiradeer@deersoftware.dev>
2023-07-08 08:29:36 -0300

    feat(lavalink): make connection timeout  configurable

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,7 @@ pub const HYDROGEN_PRIMARY_COLOR: i32 = 0x5865f2;
 pub const HYDROGEN_ERROR_COLOR: i32 = 0xf04747;
 pub const HYDROGEN_EMPTY_CHAT_TIMEOUT: u64 = 10;
 pub const HYDROGEN_QUEUE_LIMIT: usize = 1000;
+pub const LAVALINK_CONNECTION_TIMEOUT: u64 = 5000;
 
 pub static HYDROGEN_LOGO_URL: &str = "https://gitlab.com/deersoftware/hydrogen/-/raw/main/icon.png";
 
