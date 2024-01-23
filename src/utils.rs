3598fe80176a914de87b5ea3a1b8c2e46d6766d4
Nashira Deer <nashiradeer@gmail.com>
2024-01-22 21:53:11 -0300

    feat: add seek exclusive functions to utils

--- src/utils.rs
+++ src/utils.rs
@@ -80,3 +80,37 @@ pub fn error_message(i18n: &I18n, locale: &str, error: &str) -> String {
             .replace("{url}", HYDROGEN_BUG_URL)
     )
 }
+
+/// Gets a string option from a command.
+pub fn get_str_option<'a>(command: &'a CommandInteraction, index: usize) -> Option<&'a str> {
+    command.data.options.get(index)?.value.as_str()
+}
+
+/// Converts a time in seconds to a string.
+pub fn time_to_string(seconds: i32) -> String {
+    if seconds < 60 {
+        return format!("00:{:02}", seconds);
+    } else if seconds < 60 * 60 {
+        let time = seconds as f32;
+        let minutes = (time / 60.0).floor();
+        let seconds = time - minutes * 60.0;
+        return format!("{:02}:{:02}", minutes as u32, seconds as u32);
+    }
+
+    let time = seconds as f32;
+    let hours = (time / 60.0 / 60.0).floor();
+    let minutes = (time - hours * 60.0 * 60.0).floor();
+    let seconds = time - minutes * 60.0 - hours * 60.0 * 60.0;
+    return format!(
+        "{:02}:{:02}:{:02}",
+        hours as u32, minutes as u32, seconds as u32
+    );
+}
+
+/// Creates a progress bar.
+pub fn progress_bar(current: i32, total: i32) -> String {
+    let item_total = 30usize;
+    let item_count = (current as f32 / (total as f32 / item_total as f32)).round();
+    let bar = "▓".repeat(item_count as usize);
+    format!("╣{:░<width$.width$}╠", bar, width = item_total)
+}
